1777f15a2be934da2a8e88d9eb19dde02030d9c6
Amal <amal.alabdulkarim@gmail.com>
2019-04-24 20:37:31 -0400

    New England map added to page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/style.css'
+import './css/style.css';
+import "@babel/polyfill";
+import * as d3 from 'd3';
 import map from './js/map';
 import timeline from './js/timeline';
 import logMessage from './js/client';
@@ -11,7 +13,7 @@ import oneinfive from './js/oneinfive';
 import fourtypercent from './js/fourtypercent';
 import other_stats from './js/other_stats';
 import other_stats_captions from './js/other_stats_captions';
-import * as d3 from 'd3';
+import new_england_map from "./js/new_england_map";
 
 // Log message to console
 logMessage('Its finished!!');
@@ -27,6 +29,7 @@ fourtypercent(d3);
 other_stats(d3);
 other_stats_captions(d3);
 map(d3);
+new_england_map(d3);
 timeline();
 comparative_chart(d3);
 barchart(d3);
